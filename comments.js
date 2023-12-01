afc998a60e5c00fc469d512e2b09f992f6e6a4be
Cody Koscielski <60411570+codykoscielski@users.noreply.github.com>
2023-12-01 19:30:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,20 +1,25 @@
+// Create web server
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.post('/api/data', (req, res) => {
-  // Handle the POST request here
-  // You can access the request body using req.body
+app.post('/api/data', async (req, res) => {
+  try {
+    // Handle the POST request here
+    // You can access the request body using req.body
 
-  // Example response
-  const response = {
-    message: 'Received POST request',
-    data: req.body
-  };
+    // Example response
+    const response = {
+      message: 'Received POST request',
+      data: req.body
+    };
 
-  res.json(response);
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 app.listen(port, () => {
